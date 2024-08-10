95f98f8cdc944e44b48546c165f0416f9fb0e79c
Sheikh Bayazid <sheikhbayazid@gmail.com>
2024-08-10 20:37:14 +0200

    Add content to the ArticlePreviewView

--- Presentation/Sources/Presentation/Internal/News/Preview/ArticlePreviewView.swift
+++ Presentation/Sources/Presentation/Internal/News/Preview/ArticlePreviewView.swift
@@ -5,14 +5,62 @@
 //  Created by Sheikh Bayazid on 2024-08-10.
 //
 
+import AppFoundation
 import SwiftUI
 
 struct ArticlePreviewView: View {
+    var article: NewsArticle
+
+    init(article: NewsArticle) {
+        self.article = article
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 8) {
+            if let urlToImage = article.urlToImage, let imageURL = URL(string: urlToImage) {
+                AsyncImage(url: imageURL) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(.rect(cornerRadius: 12))
+                    } else if phase.error != nil {
+                        Image(systemName: "questionmark.diamond")
+                            .imageScale(.large)
+                    } else {
+                        ProgressView()
+                    }
+                }
+            }
+
+            ArticleSummeryView(article: article)
+        }
+    }
+}
+
+private struct ArticleSummeryView: View {
+    let article: NewsArticle
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(article.title)
+                .font(.headline)
+
+            if let author = article.author {
+                Text("By \(author)")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.secondary)
+            }
+
+            Text(markdown: article.descriptions)
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                .lineLimit(2)
+        }
     }
 }
 
 #Preview {
-    ArticlePreviewView()
+    ArticlePreviewView(article: .preview1)
 }
