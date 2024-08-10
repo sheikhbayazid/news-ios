84da0105b4f7ddf154a431114821c821e3f4098a
Sheikh Bayazid <sheikhbayazid@gmail.com>
2024-08-10 13:58:35 +0200

    Create ArticlePreviewView

---
+++ Presentation/Sources/Presentation/Internal/News/Preview/ArticlePreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticlePreviewView.swift
+//  Presentation
+//
+//  Created by Sheikh Bayazid on 2024-08-10.
+//
+
+import SwiftUI
+
+struct ArticlePreviewView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ArticlePreviewView()
+}
