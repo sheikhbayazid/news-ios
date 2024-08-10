ce2114a966d7d7a8af91f7256712870dc62d6403
Sheikh Bayazid <sheikhbayazid@gmail.com>
2024-08-10 12:32:05 +0200

    Initial Commit

---
+++ News/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  News
+//
+//  Created by Sheikh Bayazid on 2024-08-10.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
