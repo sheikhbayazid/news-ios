fa7d8288d05c4842f7ad87ad9d232083e3c4fbc1
Sheikh Bayazid <sheikhbayazid@gmail.com>
2024-08-10 22:53:11 +0200

    Remove comments

--- Network/Tests/NetworkTests/Mocks/MockedURLSession.swift
+++ Network/Tests/NetworkTests/Mocks/MockedURLSession.swift
@@ -6,14 +6,8 @@
 //
 
 import Foundation
-
-import Foundation
 @testable import Network
 
-/// A mock URL session for testing purposes.
-///
-/// This class conforms to `URLSessionProtocol` and allows the injection of predefined responses,
-/// data, and errors to simulate network conditions during testing.
 final class MockedURLSession: URLSessionProtocol {
     var data: Data?
     var response: URLResponse?
@@ -21,31 +15,21 @@ final class MockedURLSession: URLSessionProtocol {
 
     var delay: TimeInterval?
 
-    /// Performs a data task for a given URL request, simulating a URL session.
-    ///
-    /// - Parameter request: A URL request object that provides the URL, cache policy, request type, body data or body stream, and so on.
-    /// - Returns: A tuple containing the data and response.
-    ///
-    /// This method helps with testing by allowing the simulation of different network responses and errors.
     func dataCompatible(for request: URLRequest) async throws -> (Data, URLResponse) {
         let delay = getTimeoutDelay()
 
-        // Simulate network delay if necessary
         if delay > 0 {
             try await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
         }
 
-        // Simulate an error if provided
         if let error = error {
             throw error
         }
 
-        // Simulate a successful response with data
         if let data = data, let response = response {
             return (data, response)
         }
 
-        // If neither data nor error is provided, throw a generic error
         throw URLError(.unknown)
     }
 
