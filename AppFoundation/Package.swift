9f9601b8b4cbc2f2d5378461495dd2b1a3cbceff
Sheikh Bayazid <sheikhbayazid@gmail.com>
2024-08-10 12:35:52 +0200

    Create AppFoundation module

---
+++ AppFoundation/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppFoundation",
+    platforms: [.iOS(.v17)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AppFoundation",
+            targets: ["AppFoundation"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "AppFoundation")
+    ]
+)
