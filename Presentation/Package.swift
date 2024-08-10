10f67adcaad5c852e9534490a310573055a6893f
Sheikh Bayazid <sheikhbayazid@gmail.com>
2024-08-10 13:36:31 +0200

    Create Presentation module

--- Presentation/Package.swift
+++ Presentation/Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Domain",
+    name: "Presentation",
     platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Domain",
-            targets: ["Domain"]
+            name: "Presentation",
+            targets: ["Presentation"]
         ),
-    ],
+    ],    
     dependencies: [
         .package(path: "AppFoundation")
     ],
@@ -20,12 +20,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Domain",
+            name: "Presentation",
             dependencies: ["AppFoundation"]
         ),
         .testTarget(
-            name: "DomainTests",
-            dependencies: ["Domain"]
+            name: "PresentationTests",
+            dependencies: ["Presentation"]
         ),
     ]
 )
